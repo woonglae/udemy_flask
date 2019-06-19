019d1efdd1cd93c20316d8adea2f22b4a05c0593
Woongrae Cho <woonglae@gmail.com>
2019-06-19 14:38:02 -0400

    Add another route using dacorator of python

--- Flask/ex_flask_1.py
+++ Flask/ex_flask_1.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello Puppy!</h>'
 
+@app.route('/information')
+def info():
+    return "<h1>Puppies are cute!</h1>"
+
 if __name__ == '__main__':
     app.run()
