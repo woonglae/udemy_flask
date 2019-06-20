50e0e2c50665522df760e89e7430f5f7e2546e92
Woongrae Cho <woonglae@gmail.com>
2019-06-19 23:12:23 -0400

    Add Control flow which is for loop

--- Flask/ex_templates_1.py
+++ Flask/ex_templates_1.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     name = "Ray"
-    letters = list(name)
-    dictionary = {"name":'Ray'}
-    return render_template('ex_templates_1.html', name=name, letters=letters, dictionary=dictionary)
+    mylist = [1,2,3,4,5]
+    return render_template('ex_templates_1.html', name=name, mylist = mylist)
 
 if __name__ == '__main__':
     app.run(debug=True)
