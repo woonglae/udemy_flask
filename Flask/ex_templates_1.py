5d2baff6c90b8e237f9aa7333c577ac598c1ec1f
Woongrae Cho <woonglae@gmail.com>
2019-06-19 23:56:48 -0400

    minor fix

--- Flask/ex_templates_1.py
+++ Flask/ex_templates_1.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     name = "Ray"
-    mylist = [1,2,3,4,5]
+    mylist = ["Sally","Alissa","Jack"]
     return render_template('ex_templates_1.html', name=name, mylist = mylist)
 
 if __name__ == '__main__':
