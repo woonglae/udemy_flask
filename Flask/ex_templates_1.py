6d763c3707e7400dc1de42304658edcd3fb4d41e
Woongrae Cho <woonglae@gmail.com>
2019-06-19 22:46:39 -0400

    Using Jinja to pass variables to templates

--- Flask/ex_templates_1.py
+++ Flask/ex_templates_1.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('ex_templates_1.html')
+    some_variable = "Ray"
+    return render_template('ex_templates_1.html', my_variable = some_variable)
 
 if __name__ == '__main__':
     app.run(debug=True)
