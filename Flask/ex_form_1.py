7f3b4ccbfeb642dde44b8bb61cf03833cd874806
Woongrae Cho <woonglae@gmail.com>
2019-06-22 17:18:42 -0400

    Add 404 error page and connect

--- Flask/ex_form_1.py
+++ Flask/ex_form_1.py
@@ -16,7 +16,9 @@ def thank_you():
     last = request.args.get('last')
     return render_template('thankyou.html', first=first, last=last)
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
