6b5ce0dc7f140bb41e1aab13590efd2b00a8e5aa
Woongrae Cho <woonglae@gmail.com>
2019-06-22 16:19:14 -0400

    Connect html files to py file

--- Flask/ex_form_1.py
+++ Flask/ex_form_1.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/signup_form')
+def signup_form():
+    return render_template('signup.html')
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thankyou.html')
 
 
 
