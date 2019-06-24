c2971b086c17dd357929ae204bb0fa2d8eda1206
Woongrae Cho <woonglae@gmail.com>
2019-06-24 16:51:59 -0400

    set flash message in py file

--- Flask/ex_form_4.py
+++ Flask/ex_form_4.py
@@ -15,7 +15,7 @@ def index():
     form = SimpleForm()
     if form.validate_on_submit():
         session['breed'] = form.breed.data
-        flash('You just changed your breed to:')
+        flash('You just changed your breed to: {}'.format(session['breed']))
         return redirect(url_for('index'))
     return render_template('ex_form_4.html', form=form)
 
