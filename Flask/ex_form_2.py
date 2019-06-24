6a10ec1e9e58760f6c7b142a37784e449730d740
Woongrae Cho <woonglae@gmail.com>
2019-06-24 00:08:45 -0400

    Finish py file

--- Flask/ex_form_2.py
+++ Flask/ex_form_2.py
@@ -1,3 +1,27 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class InfoForm(FlaskForm):
+
+    breed = StringField("What Breed are you?")
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    breed = False
+
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data = ''
+
+    return render_template('ex_form_2.html', form=form, breed=breed)
+
+if __name__ = '__main__':
+    app.run(debug=True)
