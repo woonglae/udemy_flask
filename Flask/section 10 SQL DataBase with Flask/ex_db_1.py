f46108d84ab525c079b3f746f4f894a8eb5e74d6
Woongrae Cho <woonglae@gmail.com>
2019-06-26 14:22:22 -0400

    Add file for db ex 1

---
+++ Flask/section 10 SQL DataBase with Flask/ex_db_1.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+## __file__ --> ex_db_1.py
+## dirname --> flask_bootcamp/ex_db_1.py
+## abspath --> ~/udemy/flask_bootcamp/ex_db_1.py
+
+print(basedir)
