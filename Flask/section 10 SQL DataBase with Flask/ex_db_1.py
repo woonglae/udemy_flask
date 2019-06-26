75b8cda168defd3a9cc9120df8d12c019bf00b95
Woongrae Cho <woonglae@gmail.com>
2019-06-26 14:25:38 -0400

    Connect to SQL with py file

--- Flask/section 10 SQL DataBase with Flask/ex_db_1.py
+++ Flask/section 10 SQL DataBase with Flask/ex_db_1.py
@@ -8,3 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 ## dirname --> flask_bootcamp/ex_db_1.py
 ## abspath --> Users/raycho//udemy/flask_bootcamp/ex_db_1.py
 ## ==> basedir = abspath
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+
+##if you wanna track more detail
+##app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
