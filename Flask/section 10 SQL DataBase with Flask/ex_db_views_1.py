f44bbedfa00f150aae9cfefb3d20cb3a6c338d0a
Woongrae Cho <woonglae@gmail.com>
2019-06-27 21:52:49 -0400

    Set database python file

--- Flask/section 10 SQL DataBase with Flask/ex_db_views_1.py
+++ Flask/section 10 SQL DataBase with Flask/ex_db_views_1.py
@@ -1,3 +1,36 @@
  # addoption_site.py
 
- 
+ import os
+ # from forms import ADDForm, DelForm
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'mysecretkey'
+
+########### SQL DATABASE SECTION ###############
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+########### Models ############
+
+class Puppy(db.Model):
+
+    __tablename__ = 'puppies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"Puppy name : {self.name}"
