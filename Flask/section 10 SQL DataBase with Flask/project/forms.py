b78254fdbf2c17283e8c2dffec29d2de3d60d9b5
Woongrae Cho <woonglae@gmail.com>
2019-06-28 16:30:26 -0400

    Make folder to implement project

---
+++ Flask/section 10 SQL DataBase with Flask/project/forms.py
@@ -0,0 +1,18 @@
+# forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+
+class ADDForm(FlaskForm):
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+    id = IntegerField("ID Number of Puppy to Remove: ")
+    submit = SubmitField("Remove Puppy")
+
+class AddOwnerForm(FlaskForm):
+    name = StringField('Name of Owner: ')
+    id = IntegerField('Id of Puppy: ')
+    submit = SubmitField('Add Owner')
