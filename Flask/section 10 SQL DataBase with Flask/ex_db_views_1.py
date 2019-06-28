ffdbeabe6ad10053563d6ecbc88f31f4db074ac7
Woongrae Cho <woonglae@gmail.com>
2019-06-27 22:15:04 -0400

    Finish define function, now need to finish html files

--- Flask/section 10 SQL DataBase with Flask/ex_db_views_1.py
+++ Flask/section 10 SQL DataBase with Flask/ex_db_views_1.py
@@ -1,6 +1,6 @@
  # addoption_site.py
 import os
-from forms import ADDForm, DelForm
+from forms import ADDForm,  DelForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -43,3 +43,33 @@ def index():
 @app.route('/add', methods=['GET', 'POST'])
 def add_pup():
     form = ADDForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('add.html', form=form)
+
+@app.route('/list', methods=['GET', 'POST'])
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+@app.route('/delete', methods=['GET', 'POST'])
+def del_pup():
+    form = DelForm
+
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
