66c663cd1947dacc340fb15834929d3dc26e3469
Woongrae Cho <woonglae@gmail.com>
2019-06-29 22:07:21 -0400

    Add new lines to hash password using werkzeug

--- Flask/section 12 Flask Migrate for Database Migrations/sample.py
+++ Flask/section 12 Flask Migrate for Database Migrations/sample.py
@@ -1,12 +1,29 @@
-from flask_bcrypt import Bcrypt
+# using Bcrypt
+#
+# from flask_bcrypt import Bcrypt
+#
+# bcrypt = Bcrypt()
+#
+# password = 'supersecretpassword'
+#
+# hashed_password = bcrypt.generate_password_hash(password)
+#
+# print(hashed_password)
+#
+# check = bcrypt.check_password_hash(hashed_password,'supersecretpassword')
+# print(check)
 
-bcrypt = Bcrypt()
 
-password = 'supersecretpassword'
+# using Werkzeug
 
-hashed_password = bcrypt.generate_password_hash(password)
+from werkzeug.security import generate_password_hash,check_password_hash
+
+hashed_password = generate_password_hash('mypassword')
 
 print(hashed_password)
 
-check = bcrypt.check_password_hash(hashed_password,'supersecretpassword')
+check = check_password_hash(hashed_password, 'wrong')
+print(check)
+
+check = check_password_hash(hashed_password, 'mypassword')
 print(check)
