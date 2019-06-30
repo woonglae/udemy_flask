4df53fc87e0b35a55a756d6d2894c34766c2a0b6
Woongrae Cho <woonglae@gmail.com>
2019-06-29 22:02:49 -0400

    Check hashed password with wrong password

--- Flask/section 12 Flask Migrate for Database Migrations/sample.py
+++ Flask/section 12 Flask Migrate for Database Migrations/sample.py
@@ -7,3 +7,6 @@ password = 'supersecretpassword'
 hashed_password = bcrypt.generate_password_hash(password)
 
 print(hashed_password)
+
+check = bcrypt.check_password_hash(hashed_password,'wrongpassword')
+print(check)
