57cdae4806b56b1be76ae24314bfad02515f0699
Woongrae Cho <woonglae@gmail.com>
2019-07-01 00:01:43 -0400

    Finish app.py

--- Flask/section 12 Flask Migrate for Database Migrations/login/app.py
+++ Flask/section 12 Flask Migrate for Database Migrations/login/app.py
@@ -20,3 +20,39 @@ def logout():
     logout_user()
     flash("You logged out")
     return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user.check_password(form.password.data) and user is not none:
+            login_user(user)
+            flash('Logged in successfully.')
+            next = request.args.get('next')
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
+
+            return redirect(next)
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash("Thanks for registration!")
+        return redirect(ulr_for('login'))
+
+    return render_template('register.html',form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
