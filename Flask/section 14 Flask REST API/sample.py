da99fa0d7a719ddc4ef8b26fbc93a2bc690d31b6
Woongrae Cho <woonglae@gmail.com>
2019-07-01 22:53:30 -0400

    Set get method and post method

--- Flask/section 14 Flask REST API/sample.py
+++ Flask/section 14 Flask REST API/sample.py
@@ -4,4 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# {'name':"Rufus", .....}
 puppies = []
+
+class PuppyNames(Resource):
+    def get(self, name):
+        for pup in puppies:
+            if pup['name']==name:
+                return pup
+        return {'name':None}
+
+
+    def post(self, name):
+        pup = {'name':name}
+
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+        pass
